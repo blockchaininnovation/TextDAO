6ef4d02388d8b8e5bad9fcb88b3a0df58ba020fb
shibano <shibano@tmi.t.u-tokyo.ac.jp>
2025-07-11 10:16:09 +0900

    iconURI登録できた

--- src/textDAO/functions/OnboardImage.sol
+++ src/textDAO/functions/OnboardImage.sol
@@ -4,9 +4,18 @@ pragma solidity ^0.8.24;
 import {OnlyMemberBase} from "bundle/textDAO/functions/onlyMember/OnlyMemberBase.sol";
 import {Storage, Schema} from "bundle/textDAO/storages/Storage.sol";
 
-
 contract OnboardImage is OnlyMemberBase {
-    function onboardImage(string memory ipfsUrl, bytes memory signature) external returns (bool) {
+    event MemberIconRegistered(
+        uint256 indexed memberId,
+        address indexed account,
+        string iconURI,
+        bytes iconVerifiedSignature
+    );
+
+    function onboardImage(
+        string memory ipfsUrl,
+        bytes memory signature
+    ) external returns (bool) {
         Schema.MemberJoinProtectedStorage storage $member = Storage.$Members();
 
         bool isMember = false;
@@ -24,8 +33,9 @@ contract OnboardImage is OnlyMemberBase {
         require($m.addr == msg.sender, "You are not the member.");
         $m.iconURI = ipfsUrl;
         $m.iconVerifiedSignature = signature;
-        
+
+        emit MemberIconRegistered(memberId, msg.sender, ipfsUrl, signature);
+
+        return true;
     }
-
-
 }
